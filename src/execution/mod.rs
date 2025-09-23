a51a422796c7e01fcc2e19c97617e3402d73454c
CleConor <francesco98.cle@gmail.com>
2025-09-23 22:59:40 +0200

    RecordBatch implementation

--- src/execution/mod.rs
+++ src/execution/mod.rs
@@ -1,5 +1,7 @@
 pub mod array;
 pub mod schema;
+pub mod record_batch;
 
 pub use array::Array;
-pub use schema::DataType;
+pub use schema::{DataType, Field, Schema};
+pub use record_batch::{RecordBatch, RecordBatchBuilder};
